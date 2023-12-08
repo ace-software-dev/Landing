489379a4fc2ce8279bcfc2d37c464cecc20d02b4
JARE2001 <A01701350@itesm.mx>
2023-12-08 16:07:34 -0600

    fix identation

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <div className=' px-6 py-4 bg-black bg-opacity-40 justify-center items-center gap-7 inline-flex font-light'>
-          <div className="text-white text-base">© ACE Software. Todos los derechos reservados.</div>
-          <div className="w-7">|</div>
-          <div className="text-white text-base">Política de Privacidad</div>
+      <div className="text-white text-base">© ACE Software. Todos los derechos reservados.</div>
+      <div className="w-7">|</div>
+      <div className="text-white text-base">Política de Privacidad</div>
     </div>
   )
 }
