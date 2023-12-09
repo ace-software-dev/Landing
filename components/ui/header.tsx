37af1cb2b96fc333f7f9a66694fd0a0db28968fe
Felipe <feliyepez2001@gmail.com>
2023-12-08 23:17:06 -0500

    refactor: tailwind style and color

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -10,37 +10,37 @@ export default function Header() {
             <img src = "/images/AceLogo.svg" alt="ACE Logo" className="mt-2 mb-2 mr-2 ml-2" />
           </div>
         </Link>
-        <ul className="justify-center items-center gap-8 hidden md:flex space-x-2 mr-8 ml-8">
+        <ul className="justify-center items-center gap-8 hidden md:flex space-x-2 mr-8 ml-8 text-lg font-normal">
           <li>
             <Link href="/">
-              <div className="text-white text-lg font-normal font-['Filson Pro'] hover:underline">Inicio</div>
+              <div className=" hover:underline">Inicio</div>
             </Link>
           </li>
           <li>
             <Link href="/">
-              <div className="text-white text-lg font-normal font-['Filson Pro'] hover:underline">Nosotros</div>
+              <div className="hover:underline">Nosotros</div>
             </Link>
           </li>
           <li>
             <Link href="/">
-              <div className="text-white text-lg font-normal font-['Filson Pro'] hover:underline">Servicios</div>
+              <div className="hover:underline">Servicios</div>
             </Link>
           </li>
           <li>
             <Link href="/">
-              <div className="text-white text-lg font-normal font-['Filson Pro'] hover:underline">Clientes</div>
+              <div className="hover:underline">Clientes</div>
             </Link>
           </li>
         </ul>
 
         <div className="justify-start items-center gap-6 flex">
-          <div className="hidden lg:flex ml-2">
+          <div className="hidden lg:flex ml-2 text-base">
             <Link href="/">
-              <span className="text-white text-base font-['Filson Pro']">EN</span>
+              <span>EN</span>
             </Link>
-            <span className="text-white text-base font-['Filson Pro']"> / </span>
+            <span> / </span>
             <Link href="/">
-              <span className="text-white text-base font-bold font-['Filson Pro']">ES</span>
+              <span className="font-bold">ES</span>
             </Link>
           </div>
           <Link href="">
