a32afe4b09a2d1b30d35e6711afb426b915bdf3a
Felipe <feliyepez2001@gmail.com>
2023-12-11 21:12:32 -0500

    refactor: button style

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ export default function Button({ children, classes }: ButtonProps) {
     <div className={`
       px-6 py-3 bg-gradient-to-l from-purple-600 to-blue-600 rounded-full
       justify-center items-center inline-flex cursor-pointer hover:opacity-90
-      text-2xl font-bold ${classes}
+      text-2xl font-semibold text-white ${classes}
     `}>
         {children}
     </div>
