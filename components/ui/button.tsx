d2efe04736b1ec588502c68b923108f08ad48598
JARE2001 <A01701350@itesm.mx>
2023-12-28 17:58:35 -0600

    refactor:  add a go to option to the boton component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,15 +3,19 @@ import React from 'react'
 interface ButtonProps {
   children: React.ReactNode,
   classes?: string,
+  goTo?: string,
 }
 
-export default function Button({ children, classes }: ButtonProps) {
+export default function Button({ children, classes, goTo }: ButtonProps) {
+  const openInNewTab = (url: string) => {
+    window.open(url, "_blank", "noreferrer");
+  };
   return (
     <div className={`
       px-6 py-3 bg-gradient-to-l from-purple-600 to-blue-600 rounded-full
       justify-center items-center inline-flex cursor-pointer hover:opacity-90
       text-2xl font-semibold text-white ${classes}
-    `}>
+    `} onClick={() => goTo ? openInNewTab(goTo) : null}>
         {children}
     </div>
   )
