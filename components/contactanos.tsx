a9d53083292f83b6c9a7d803b2370fdcce1fc160
JARE2001 <A01701350@itesm.mx>
2023-12-30 13:04:17 -0600

    feat: add contry code dropdown

--- components/contactanos.tsx
+++ components/contactanos.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import Button from './ui/button'
 import Title from './ui/title'
-import OrangeRocket from '@/public/images/Rocket.svg'
 
 export default function Contactanos() {
     return (
@@ -14,7 +13,7 @@ export default function Contactanos() {
                     </div>
                 </div>
                 <div className="justify-center items-center gap-24 inline-flex">
-                    <img className="w-[234px] h-[300px]" src='\images\CoolRocket.svg' />
+                    <img className=" hidden md:block w-[234px] h-[300px]" src='\images\CoolRocket.svg' />
                     <form>
                         <div className="w-[760px] flex-col justify-start items-center gap-7 inline-flex">
                             <div className="self-stretch h-[87px] flex-col justify-start items-start gap-2 flex">
@@ -23,15 +22,24 @@ export default function Contactanos() {
                             </div>
                             <div className="self-stretch h-[87px] flex-col justify-start items-start gap-2 flex">
                                 <label className="text-slate-50 text-base font-medium">Correo*</label>
-                                <input type="email" id="correo" name="correo" placeholder="ace@gmail.com" required className="self-stretch px-4 py-[18px] bg-slate-700 rounded-xl gap-2.5 text-slate-50 text-xl focus:outline-none" />
+                                <input type="email" id="correo" name="correo" placeholder="ace@gmail.com" required className="self-stretch px-4 py-[18px] bg-slate-700 rounded-xl gap-2.5 text-slate-50 text-xlfocus:ring-2 focus:ring-inset focus:ring-indigo-600" />
                             </div>
                             <div className="self-stretch h-[89px] flex-col justify-start items-start gap-2 flex">
                                 <label className="text-slate-50 text-base font-medium">WhatsApp</label>
-                                <input type="text" id="numero" name="numero" placeholder="444 123 4444" required className="self-stretch px-4 py-[18px] bg-slate-700 rounded-xl gap-2.5 text-slate-50 text-xl focus:outline-none" />
+                                <div className="flex w-full">
+                                    <div className="relative inline-block">
+                                        <select  id="country-code" name="country-code" className="form-select px-4 py-[18px] bg-slate-700 rounded-l-xl text-slate-50 text-xl focus:ring-2 focus:ring-inset focus:ring-indigo-600 w-24">
+                                            <option value="52" defaultValue={1}>+52</option>
+                                            <option value="1">+1</option>
+                                        </select>
+                                    </div>
+                                    <input type="text" id="numero" name="numero" placeholder="123 456 7890" required className="w-full self-stretch px-4 py-[18px] bg-slate-700 rounded-r-xl gap-2.5 text-slate-50 text-xl focus:ring-2 focus:ring-inset focus:ring-indigo-600" />
+                                </div>
                             </div>
+
                             <div className="self-stretch h-[187px] flex-col justify-start items-start gap-2 flex">
                                 <label className="text-slate-50 text-base font-medium">Cuéntanos un poco más de tu idea</label>
-                                <input type="text" id="numero" name="numero" placeholder="Escribe aquí..." required className="self-stretch h-40 px-4 py-[18px] bg-slate-700 rounded-xl placeholder:justify-top placeholder:items-start gap-2.5 inline-flex text-slate-50 text-xl focus:outline-none" />
+                                <input type="text" id="numero" name="numero" placeholder="Escribe aquí..." required className="self-stretch h-40 px-4 py-[18px] bg-slate-700 rounded-xl justify-top items-start gap-2.5 inline-flex text-slate-50 text-xl focus:ring-2 focus:ring-inset focus:ring-indigo-600" />
                             </div>
                             <Button classes='px-8 py-4  text-slate-50 text-lg font-bold'>Contáctanos</Button>
                         </div>
