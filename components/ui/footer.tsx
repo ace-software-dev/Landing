4144ce2c0682ef973a97639a293a92bf8caa3469
JARE2001 <A01701350@itesm.mx>
2023-12-30 22:02:21 -0600

    add: footer translation

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,12 +1,17 @@
+'use client'
 import React from 'react'
 import Link from 'next/link'
+import getDictionary from '@/app/dictionaries'
+
 
 export default function Footer() {
+  const dict = getDictionary()
+
   return (
     <div className='mt-6 px-6 py-4 bg-black bg-opacity-40 justify-center items-center gap-7 inline-flex font-light'>
-      <div>© ACE Software. Todos los derechos reservados.</div>
+      <div>© ACE Software. {dict.footer.title}</div>
       <div className="w-7">|</div>
-      <div>Política de Privacidad</div>
+      <div>{dict.footer.privacy}</div>
     </div>
   )
 }
