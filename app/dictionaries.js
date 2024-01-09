c88cab3cd170710c981116a7a61705c9f08244d2
JR-espacial <jorge_alan_2001@hotmail.com>
2024-01-08 19:44:41 -0600

    fix: get language with cookie

--- app/dictionaries.js
+++ app/dictionaries.js
@@ -8,6 +8,26 @@ const locales = {
   es,
 };
 
-export default function getDictionary(locale) {
+// Function to get the value of a specific cookie
+function getCookie(name) {
+  // Split the cookies into an array
+  let cookies = document.cookie.split('; ');
+
+  // Iterate through the cookies to find the one with the specified name
+  for (let i = 0; i < cookies.length; i++) {
+    let cookie = cookies[i].split('=');
+    if (cookie[0] === name) {
+      // Return the value of the cookie
+      return cookie[1];
+    }
+  }
+
+  // If the cookie is not found, return null
+  return 'es';
+}
+
+export default function getDictionary() {
+  locale = getCookie('NEXT_LOCALE');
+  console.log('locale', locale)
   return locales[locale] || es;
 }
